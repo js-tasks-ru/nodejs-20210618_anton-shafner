5be4377cad61a2a08d1f589911bec6baae9fedc1
dragooon4ik24 <shafner.anton@gmail.com>
2021-12-06 22:15:00 +0300

    add solution for 03-module/02-task

--- 03-streams/02-line-split-stream/LineSplitStream.js
+++ 03-streams/02-line-split-stream/LineSplitStream.js
@@ -4,12 +4,29 @@ const os = require('os');
 class LineSplitStream extends stream.Transform {
   constructor(options) {
     super(options);
+    this.endStr = '';
   }
 
   _transform(chunk, encoding, callback) {
+    let str = chunk.toString('utf-8');
+    if (this.endStr) {
+      str = this.endStr + str;
+    }
+    const arr = str.split(os.EOL);
+    if (!str.endsWith(os.EOL)) {
+      const lastIndex = arr.length - 1;
+      this.endStr = arr[lastIndex];
+      arr.splice(lastIndex, 1);
+    }
+    for (const str of arr) {
+      this.push(str);
+    }
+    callback();
   }
 
   _flush(callback) {
+    this.push(this.endStr);
+    callback();
   }
 }
 
